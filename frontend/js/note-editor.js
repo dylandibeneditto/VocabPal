2ef5df1e856617a42112a8401ff0007c3d12a5bf
dylan <justbeawesome9@gmail.com>
2023-11-19 21:38:29 -0500

    added monospace and h2 rich text

--- frontend/js/note-editor.js
+++ frontend/js/note-editor.js
@@ -6,11 +6,43 @@ export default class NoteEditor {
         this.selection = window.getSelection();
         this.text = 0
         document.getElementById("header1").addEventListener("mousedown", () => {
-            this.addHeader();
+            this.addRich("h1");
+        })
+        document.getElementById("header2").addEventListener("mousedown", () => {
+            this.addRich("h2");
+        })
+        document.getElementById("monospace").addEventListener("mousedown", () => {
+            this.addMonospace()
         })
     }
 
-    addHeader() {
+    addRich(name) {
+
+        if (this.selection.rangeCount > 0) {
+            const range = this.selection.getRangeAt(0);
+            const selectedNode = range.commonAncestorContainer;
+            console.log(selectedNode.parentElement);
+
+            if (selectedNode.nodeType === Node.TEXT_NODE) {
+                const parentElement = selectedNode.parentElement;
+
+                // Toggle between h1 and normal text
+                if (parentElement.nodeName === name) {
+                    const textNode = document.createTextNode(parentElement.innerText);
+                    parentElement.parentNode.replaceChild(textNode, parentElement);
+                } else {
+                    // Wrap the selected line in an h1 element
+                    const element = document.createElement(name);
+                    element.appendChild(document.createTextNode(selectedNode.nodeValue));
+                    selectedNode.remove()
+                    range.deleteContents();
+                    range.insertNode(element);
+                }
+            }
+        }
+    }
+
+    addMonospace() {
         if (this.selection.rangeCount > 0) {
             const range = this.selection.getRangeAt(0);
             const selectedNode = range.commonAncestorContainer;
@@ -18,18 +50,16 @@ export default class NoteEditor {
             if (selectedNode.nodeType === Node.TEXT_NODE) {
                 const parentElement = selectedNode.parentElement;
 
-                console.log(range)
-                // Toggle between h1 and normal text
-                if (parentElement.nodeName === 'H1') {
+                if (parentElement.classList.contains("monospace")) {
                     const textNode = document.createTextNode(parentElement.innerText);
                     parentElement.parentNode.replaceChild(textNode, parentElement);
                 } else {
-                    // Wrap the selected line in an h1 element
-                    const h1Element = document.createElement('h1');
-                    h1Element.appendChild(document.createTextNode(selectedNode.nodeValue));
+                    const element = document.createElement("div");
+                    element.classList.add("monospace");
+                    element.appendChild(document.createTextNode(selectedNode.nodeValue));
                     selectedNode.remove()
-                    range.deleteContents();
-                    range.insertNode(h1Element);
+                    range.deleteContents()
+                    range.insertNode(element);
                 }
             }
         }
