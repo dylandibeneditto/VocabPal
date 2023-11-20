c8e112e2ed4eee5b6c39bfc4d58e804bd69faa23
dylan <justbeawesome9@gmail.com>
2023-11-19 21:23:19 -0500

    fixed header issues

--- frontend/js/note-editor.js
+++ frontend/js/note-editor.js
@@ -5,7 +5,7 @@ export default class NoteEditor {
         this.p = p;
         this.selection = window.getSelection();
         this.text = 0
-        this.p.addEventListener("mousedown", () => {
+        document.getElementById("header1").addEventListener("mousedown", () => {
             this.addHeader();
         })
     }
@@ -14,22 +14,24 @@ export default class NoteEditor {
         if (this.selection.rangeCount > 0) {
             const range = this.selection.getRangeAt(0);
             const selectedNode = range.commonAncestorContainer;
-        
+
             if (selectedNode.nodeType === Node.TEXT_NODE) {
-              const parentElement = selectedNode.parentElement;
-        
-              // Toggle between h1 and normal text
-              if (parentElement.nodeName === 'H1') {
-                const textNode = document.createTextNode(parentElement.innerText);
-                parentElement.parentNode.replaceChild(textNode, parentElement);
-              } else {
-                // Wrap the selected line in an h1 element
-                const h1Element = document.createElement('h1');
-                h1Element.appendChild(document.createTextNode(selectedNode.nodeValue));
-                range.deleteContents();
-                range.insertNode(h1Element);
-              }
+                const parentElement = selectedNode.parentElement;
+
+                console.log(range)
+                // Toggle between h1 and normal text
+                if (parentElement.nodeName === 'H1') {
+                    const textNode = document.createTextNode(parentElement.innerText);
+                    parentElement.parentNode.replaceChild(textNode, parentElement);
+                } else {
+                    // Wrap the selected line in an h1 element
+                    const h1Element = document.createElement('h1');
+                    h1Element.appendChild(document.createTextNode(selectedNode.nodeValue));
+                    selectedNode.remove()
+                    range.deleteContents();
+                    range.insertNode(h1Element);
+                }
             }
-          }
         }
+    }
 }
