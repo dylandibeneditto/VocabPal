93c558aa5d7e875e03754dd628fb5557d544ce07
dylan <justbeawesome9@gmail.com>
2023-11-26 18:28:01 -0500

    integrated quill.js for note editor

--- frontend/js/experience.js
+++ frontend/js/experience.js
@@ -1,6 +1,5 @@
 import PseudoID from './utils/pseudo-id.js'
 import Notes from './notes.js'
-import NoteEditor from './note-editor.js'
 
 export default class Experience {
     static instance;
@@ -12,6 +11,11 @@ export default class Experience {
         this.idGen = new PseudoID()
 
         this.notes = new Notes()
-        this.noteEditor = new NoteEditor(document.getElementById("noteEditor"));
+        this.noteEditor = new Quill('#noteEditor', {
+            modules: {toolbar: '#toolbar'},
+            placeholder: "new note...",
+            theme: 'snow',
+        });
+        //this.noteEditor = new NoteEditor(document.getElementById("noteEditor"));
     }
 }
