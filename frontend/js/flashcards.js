bff2bec226bbdc39357419f993d08bb01525630d
dylan <justbeawesome9@gmail.com>
2023-11-29 21:48:53 -0500

    get next learn quiz flashcard logic implemented

--- frontend/js/flashcards.js
+++ frontend/js/flashcards.js
@@ -2,17 +2,44 @@ class Flashcard {
     constructor(term, definition) {
         this.term = term;
         this.definition = definition;
+        this.generalMemValue = 0;
+        this.learnVal = 0;
     }
 }
 
 export default class Flashcards {
     constructor() {
-        this.flashcards = [];
+        this.flashcards = [new Flashcard("hello", "1"), new Flashcard("hello", "2"), new Flashcard("hello", "3")];
+        console.log(this.getNextLearn(this.getRandom()))
     }
 
+    //adds flashcard to end of array
     addFlashcard(term, def) {
         this.flashcards.push(new Flashcard(term, def))
     }
 
-    
+    //expect flashcard class    O(n)
+    getNextLearn(flashcard) {
+        flashcard.learnVal++;
+
+        let result = []
+        let least = 0
+
+        for (let i = 0; i < this.flashcards.length; i++) {
+            if (this.flashcards[i].learnVal < least) {
+                least = this.flashcards[i].learnVal;
+            }
+        }
+
+        for (let i = 0; i < this.flashcards.length; i++) {
+            result.push(this.flashcards[i])
+        }
+
+        return result[Math.floor(Math.random() * result.length)]
+    }
+
+    //fetches random flashcard
+    getRandom() {
+        return this.flashcards[Math.floor(Math.random()) * this.flashcards.length]
+    }
 }
