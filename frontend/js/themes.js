8ae21fd287340cb6db839995a5c0cad8e987ad4d
dylan <justbeawesome9@gmail.com>
2023-12-02 21:13:58 -0500

    theme selector visibility logic

--- frontend/js/themes.js
+++ frontend/js/themes.js
@@ -1,10 +1,23 @@
 export default class Themes {
     constructor(themes) {
         this.themes = themes;
+        this.btn = document.getElementById("themebtn");
+        this.selector = document.getElementById("themeselector")
         this.initListeners()
     }
 
     initListeners() {
-        
+        this.btn.addEventListener("mousedown", (e) => {
+            this.selector.style.display = 'flex';
+            this.selector.style.opacity = 1.0;
+        })
+
+        document.addEventListener("mousedown", (e) => {
+            console.log(e.target)
+            if(e.target.parentElement!==this.btn&&e.target.parentElement!==this.selector) {
+                this.selector.style.display = 'none';
+                this.selector.style.opacity = 0;
+            }
+        })
     }
 }
