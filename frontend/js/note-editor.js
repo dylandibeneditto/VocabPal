7fcc49caa1be25d643b14b4bccb46cdcb1a73905
dylan <justbeawesome9@gmail.com>
2024-01-07 22:52:05 -0500

    switching to rely on contenteditable

--- frontend/js/note-editor.js
+++ frontend/js/note-editor.js
@@ -1,6 +1,37 @@
 export default class NoteEditor {
     constructor(dom) {
         this.dom = dom;
-        
+        this.focused = false;
+
+
+        this.caret = 0;
+
+
+        document.addEventListener("mousedown", (e) => {
+            if(e.target == this.dom) {
+                this.focused = true;
+            } else {
+                this.focused = false;
+            }
+        })
+
+        document.addEventListener("keydown", (e) => {
+            if(this.focused && (e.ctrlKey || e.metaKey)) {
+                switch(e.key) {
+
+                }
+            }
+        })
+
+    }
+
+    updateCaret() {
+        const selection = window.getSelection();
+        const range = selection.getRangeAt(0);
+        const clonedRange = range.cloneRange();
+        clonedRange.selectNodeContents(this.dom);
+        clonedRange.setEnd(range.endContainer, range.endOffset);
+        this.caret = clonedRange.toString().length;
+        console.log("Caret Position:", this.caret);
     }
 }
